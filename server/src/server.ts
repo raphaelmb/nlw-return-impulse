e7cfe83ce505bf448fe7623074ae59857bb6668b
Raphael Berger <raphaelmb@gmail.com>
2022-05-05 21:40:49 -0300

    Fixes + Backend parte 01

---
+++ server/src/server.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import { prisma } from "./prisma";
+import nodemailer from "nodemailer";
+
+const app = express();
+
+app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "282322e02db548",
+    pass: "015121414c7d10",
+  },
+});
+
+app.post("/feedbacks", async (request, response) => {
+  const { type, comment, screenshot } = request.body;
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  await transport.sendMail({
+    from: "Equipe Feedget <oi@feedget.com>",
+    to: "Raphael Berger <raphaelmb@gmail.com>",
+    subject: "Novo feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`,
+    ].join("\n"),
+  });
+  return response.status(201).json({ data: feedback });
+});
+
+app.listen(3333, () => console.log("Server is running."));
